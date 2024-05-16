68650c883a0481cc46c7a0b65474c87b78b5ab00
andrewsmyth09 <asmyth@gmx.com>
2024-05-16 15:10:53 +0200

    Update

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,30 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    };
+
+    addPhraseToDisplay() {
+        let displayString = '';
+        this.phrase.split('').forEach(letter => {
+            letter.match(/[a-z]/i) ? 
+            displayString += `<li class="hide letter ${letter}">${letter}</li>` 
+            : 
+            displayString += '<li class="space"> </li>';
+        });
+        return displayString;
+    };
+
+    checkLetter() {
+        return 'checkLetter method works!'
+    };
+
+    showMatchedLetter() {
+        return 'showMatchedLetter method works!'
+    };
+};
+
+let test = new Phrase('This is a drag.');
