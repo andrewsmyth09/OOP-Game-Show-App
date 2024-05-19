dbefc0493bf6ab561bddc86caf686724847c8f2c
andrewsmyth09 <asmyth@gmx.com>
2024-05-19 20:20:39 +0200

    Update

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,37 @@
-document.querySelector("#overlay").style.display = "none";
+// document.querySelector("#overlay").style.display = "none";
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            new Phrase('Break a leg'),
+            new Phrase('A piece of cake'),
+            new Phrase('An arm and a leg'),
+            new Phrase('Bite the bullet'),
+            new Phrase('Best of both worlds')
+        ];
+        this.activePhrase = null;
+    };
+
+    startGame() {
+        document.querySelector("#overlay").style.display = "none";
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    };
+
+    getRandomPhrase() {
+        const randomNumber = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[randomNumber];
+    };
+
+    handleInteraction() {
+        this.activePhrase.checkLetter();
+    };
+    removeLife() {};
+    checkForWin() {};
+    gameOver() {};
+};
+
+const game = new Game();
+game.startGame();
+game.handleInteraction();
